32c1b7e15b1fb0c7434be8e1200291de7e63fb89
Jon Rowe <hello@jonrowe.co.uk>
2012-03-15 10:06:15 +0000

    this app will break under rails 3.2.2

---
+++ config/application.rb
@@ -0,0 +1,30 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
+
+module BreakDirExample
+  class Application < Rails::Application
+
+    config.autoload_paths += %W(
+      #{config.root}/services
+      #{config.root}/constraints
+      #{config.root}/really,important,libs
+    )
+
+    # Configure the default encoding used in templates for Ruby 1.9.
+    config.encoding = "utf-8"
+
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
+
+    # Enable the asset pipeline
+    config.assets.enabled = false
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BreakDirExample::Application.config.secret_token = 'b5020bb4965317e900ee1eb14aaa823828753e08fc177bb7f0beff71d8a594874259d63861841d6f232ff53e7c860acf0fb6d9ad00ba39b89e21a13a2cea765e'
